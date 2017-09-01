173a553fc95514c0d92c22cd4b715ebb2d911813
Cecily2 <cecily.downs@gmail.com>
2017-09-01 07:42:14 -0700

    Add List class with #initialize and #add_to_list

---
+++ lib/gutenberg_top_books/list.rb
@@ -0,0 +1,17 @@
+class List
+
+  def initialize(*list)
+    @list = []
+    add_to_list(list) unless list == nil
+  end
+
+  def add_to_list(list)
+    
+  end
+
+# Receives info from scraper
+# Processes it
+# Creates books (OR tells the book class to do this)
+# Creates list of those books (for CLI to print)
+
+end
