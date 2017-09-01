2dd9887b72a0ed7c29a31e58c8b0b245ea196c7c
Cecily2 <cecily.downs@gmail.com>
2017-09-01 15:56:28 -0700

    Add @link instance variable

--- lib/gutenberg_top_books/book.rb
+++ lib/gutenberg_top_books/book.rb
@@ -1,9 +1,10 @@
 class GutenbergTopBooks::Book
-  attr_accessor :name, :author, :html, :epub, :kindle
+  attr_accessor :name, :author, :link, :html, :epub, :kindle
 
-  def initialize(name, author)
+  def initialize(name, author, link)
     @name = name
     @author = author
+    @link = link
   end
 
   def add_download_urls(attributes)
