5832fc1c99172db078e3f1d30e08122f2522cfd3
Cecily2 <cecily.downs@gmail.com>
2017-09-01 07:33:35 -0700

    Add add_download_urls method

--- lib/gutenberg_top_books/book.rb
+++ lib/gutenberg_top_books/book.rb
@@ -1,8 +1,15 @@
 class Book
-  attr_accessor :name, :author, :download_urls
+  attr_accessor :name, :author, :html, :epub, :kindle
+
   def initialize(name, author)
     @name = name
     @author = author
   end
 
+  def add_download_urls(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+  
 end
