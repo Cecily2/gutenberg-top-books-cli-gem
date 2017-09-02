02fd98d47bfeb873041f49b82c2080540f5b76d5
Cecily2 <cecily.downs@gmail.com>
2017-09-01 19:04:33 -0700

    Refactor code. Account for possibility of no author

--- lib/gutenberg_top_books/scraper.rb
+++ lib/gutenberg_top_books/scraper.rb
@@ -1,25 +1,17 @@
 class GutenbergTopBooks::Scraper
 
   def self.scrape_list(time, count)
-    doc = Nokogiri::HTML(open("https://www.gutenberg.org/browse/scores/top/index.html", "Accept-Encoding" => ""))
-    # Look at correct list based on TIME argument
-    # "7", "30", or "yesterday"
-
-    correct_list = nil
-    if count == 1
-      correct_list = doc.css("h2#books-last1 + ol").first.children
-    elsif count == 7
-      correct_list = doc.css("h2#books-last7 + ol").first.children
-    else
-      correct_list = doc.css("h2#books-last30 + ol").first.children
-    end
-
     books_array = []
-
-    correct_list.css("li")[0 .. count-1].each do |book|
-      book_info = book.css("a").text.split(" by ")
-      title = book_info[0]
-      author = book_info[1].split(" (")[0]
+    doc = Nokogiri::HTML(open("https://www.gutenberg.org/browse/scores/top/index.html", "Accept-Encoding" => ""))
+    doc.css("h2#books-last#{time} + ol li")[0 .. count-1].each do |book|
+      if book.css("a").text.include?(" by ")
+        book_info = book.css("a").text.split(" by ")
+        title = book_info[0]
+        author = book_info[1].split(" (")[0]
+      else
+        title = book.css("a").text.split(" (")[0]
+        author = "N/A"
+      end
       link = book.css("a").attribute("href").value
       books_array << { :title => title, :author => author, :link => link }
     end
