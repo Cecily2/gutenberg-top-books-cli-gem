f663b123046486fe8cf0d7fcf3f1d6da4f635d0a
Cecily2 <cecily.downs@gmail.com>
2017-09-01 19:42:36 -0700

    Add comments and make variable names clearer

--- lib/gutenberg_top_books/cli.rb
+++ lib/gutenberg_top_books/cli.rb
@@ -1,24 +1,33 @@
 class GutenbergTopBooks::CLI
 
   def run
+    # Get time period and item count preferences from user
     time = choose_data_time
     count = choose_title_count
+
+    # Scrape data, add to list, and print a list of titles
     data = GutenbergTopBooks::Scraper.scrape_list(time, count)
     list = GutenbergTopBooks::List.new(data)
     list.print_titles
-    title_number = view_download_links
-    title_object = list.get_book_by_number(title_number)
-    title_download_links = GutenbergTopBooks::Scraper.scrape_download_links(title_object.link)
-    title_object.add_download_urls(title_download_links)
+
+    # Allow user to view download options by book number
+    book_number = view_download_links
+
+    # Add download links to book object
+    book_object = list.get_book_by_number(book_number)
+    book_download_links = GutenbergTopBooks::Scraper.scrape_download_links(book_object.link)
+    book_object.add_download_urls(book_download_links)
+
+    # Allow user to choose a format to download
     download_choice = choose_download_options
-    open_in_browser(title_object, download_choice)
+    open_in_browser(book_object, download_choice)
   end
 
   def choose_data_time
     puts "When would you like to view data for?"
     puts "Yesterday  |  Last 7 Days   |  30 Days"
     time = gets.strip
-    if time.downcase == "yesterday"
+    if time == "1" || time.downcase == "yesterday"
       answer = 1
     elsif time.include?("7") || time.downcase.include?("seven")
       answer = 7
